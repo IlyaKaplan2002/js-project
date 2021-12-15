a960eb67c35a84bd763eb7d50b055e321ff49b30
VorobyovaAnna <anyasonn1k@gmail.com>
2021-12-15 21:03:57 +0200

    добавлен кастомный класс

--- src/js/cards/renderMarkup.js
+++ src/js/cards/renderMarkup.js
@@ -6,14 +6,14 @@ export { renderMarkup };
 function renderMarkup(films) {
   const markup = films.reduce((acc, film) => {
     acc += `<li class="card-set-item">
-        <button href="#" target="_blank" rel="noopener noreferrer">
+        <button type=button data-id='${film.id}'>
             <img src="${MOVIE_POSTER_URL}${film.poster_path}" alt="${film.original_title}" />
             <div>
                 <h2>
                     ${film.original_title}
                 </h2>
                 <p>
-                    ${film.genres}
+                    ${film.genres.join(', ')}
                     |
                     ${film.release_date.split('-')[0]}
                     ${film.vote_average}
