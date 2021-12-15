e81d92bcef7b9cbb70dd4060a05ce7d52c88676f
NadiiaHonchar <17nmg82@gmail.com>
2021-12-15 23:53:06 +0200

    added open/ close modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import { makeNavList } from './js/header/nav';
+// import {renderModalStudents} from './js/modalStudents';
+import {onOpenModalStudents} from './js/modalStudents';
 import './sass/main.scss';
 
+// import modalStudents from './handlebars/modalStudents.hbs'
 makeNavList();
+// renderModalStudents();
+onOpenModalStudents();
+// console.log(modalStudents);
+
+
+
+
