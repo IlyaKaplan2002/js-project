f127d2d21c82a87f718b691b0a84664ebee928fa
NadiiaHonchar <17nmg82@gmail.com>
2021-12-15 14:14:37 +0200

    Added handlebars file

---
+++ src/js/modalStudents.js
@@ -0,0 +1,10 @@
+import './sass/main.scss';
+// import '../sass/base/_modal-students.scss'
+
+const refs = 
+{
+    // openModal: document.querySelector('.'),
+    closeModal: document.querySelector('.closeModal'),
+    backdropStudents: document.querySelector('.js-backdropStudents'),
+}
+
