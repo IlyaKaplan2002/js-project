861372b8e3dbcbdf2906ae530c88e4fbc562f24d
Kaplan Ilya <kaplan11092002@gmail.com>
2021-12-17 20:17:54 +0200

    universal modal fixed

--- src/js/modalButton.js
+++ src/js/modalButton.js
@@ -1,19 +1,22 @@
 export const modals = ({ openButton, closeButton, backdrop }) => {
-    backdrop.classList.add('backdrop');
-    closeButton.classList.add('.modal__close');
-    closeButton.querySelector('use').classList.add('modal__icon');
-    closeButton.querySelector('svg').classList.add('modal__icon-wrapper');
-
+  backdrop.classList.add('backdrop');
+  closeButton.classList.add('modal__close');
+  closeButton.querySelector('use').classList.add('modal__icon');
+  closeButton.querySelector('svg').classList.add('modal__icon-wrapper');
 
   const closeModal = e => {
     backdrop.classList.remove('is-open');
-    
+    document.removeEventListener('keydown', onEscClick);
   };
 
   const onEscClick = e => {
     if (e.key !== 'Escape') return;
-    closeAuthModal();
-    document.removeEventListener('keydown', onEscClick);
+    closeModal();
+  };
+
+  const onBackdropClick = e => {
+    if (e.target !== e.currentTarget) return;
+    closeModal();
   };
 
   const openModal = e => {
@@ -22,4 +25,5 @@ export const modals = ({ openButton, closeButton, backdrop }) => {
   };
   openButton.addEventListener('click', openModal);
   closeButton.addEventListener('click', closeModal);
+  backdrop.addEventListener('click', onBackdropClick);
 };
