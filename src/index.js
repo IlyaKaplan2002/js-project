c7a3c648f61880d8ec948cc42ef9c2cb6b2334e9
mandrykk <86601388+mandrykk@users.noreply.github.com>
2021-12-20 19:10:58 +0200

    created scroll-up button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { makeNavList } from './js/header/nav';
 
 import { makeTrendingMovies } from './js/main/makeTrendingMovies';
 
+import { handleScroll, scrollToTop } from './js/scroll-up';
+
 makeNavList();
 
 makeTrendingMovies();
